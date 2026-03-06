97488edb69634987db4e91f08991c5ac684e3e64
HebeleKedi <muhammedefekayha@gmail.com>
2026-03-06 17:41:06 +0300

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,9 @@
+yas = 19
+Ad = "Sıfırdan Python Öğreniyorum"
+ortalama = 98.4
+ehliyet = True
+
+print(f"Yaş:{yas} bir int değerdir")
+print(f"Ad:{Ad} bir string değerdir")
+print(f"Ortalama:{ortalama} bir float değerdir")
+print(f"Ehliyet Durumu:{ehliyet} bir bool değerdir")
